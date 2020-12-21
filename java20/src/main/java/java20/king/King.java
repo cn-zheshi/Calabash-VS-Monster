792362a79defc8d5bb5ccd316998b1854ff13d27
潘昱光 <1760864645@qq.com>
2020-12-21 17:44:52 +0800

    新建几个王的类

--- java20/src/main/java/java20/king/King.java
+++ java20/src/main/java/java20/king/King.java
@@ -6,18 +6,24 @@ import java20.tools.KindOfCreature;
 import java20.tools.Position;
 
 public abstract class King {
+    /**
+     * one kind of creature have a num as a signal
+     */
+    KindOfCreature kind;
     Ability ability;
     MoveStrategy moveStrategy;
-    KindOfCreature kindOfCreature;
     Position p;
+    
     protected King(Ability ability,MoveStrategy strategy,KindOfCreature kindOfCreature,Position p){
         this.ability=ability;
         this.moveStrategy=strategy;
-        this.kindOfCreature=kindOfCreature;
+        this.kind=kindOfCreature;
         this.p=p;
     }
+
     public boolean canUseAbility(){
         return ability.canDoSth();
     }
+
     abstract public void useAbility();
 }
