91a4e29a3f631e51d0ce74ed21521ac0490c33c4
潘昱光 <1760864645@qq.com>
2020-12-21 17:30:06 +0800

    修改王的设计

--- java20/src/main/java/java20/king/King.java
+++ java20/src/main/java/java20/king/King.java
@@ -2,8 +2,22 @@ package java20.king;
 
 import java20.ablility.*;
 import java20.movestrategy.MoveStrategy;
+import java20.tools.KindOfCreature;
+import java20.tools.Position;
 
-public class King {
+public abstract class King {
     Ability ability;
     MoveStrategy moveStrategy;
+    KindOfCreature kindOfCreature;
+    Position p;
+    protected King(Ability ability,MoveStrategy strategy,KindOfCreature kindOfCreature,Position p){
+        this.ability=ability;
+        this.moveStrategy=strategy;
+        this.kindOfCreature=kindOfCreature;
+        this.p=p;
+    }
+    public boolean canUseAbility(){
+        return ability.canDoSth();
+    }
+    abstract public void useAbility();
 }
