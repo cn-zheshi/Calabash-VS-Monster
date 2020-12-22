9e0517d9c1a224423dfc6656b0f7e499f837d274
潘昱光 <1760864645@qq.com>
2020-12-22 15:36:39 +0800

    update Main

--- java20/src/main/java/java20/Main.java
+++ java20/src/main/java/java20/Main.java
@@ -1,7 +1,17 @@
 package java20;
 
+import java.util.ArrayList;
+
+import java20.king.King;
+import java20.warrior.Creature;
+
 public class Main {
+    ArrayList<Creature> creatures;
+    ArrayList<King> kings;
     public static void main(String[] args) {
 
+    }
+    Main(){
+        
     }
 }
