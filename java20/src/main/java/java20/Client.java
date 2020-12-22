ee7ac61e6ca0e5f9ec281aec8a033bf6108ef526
潘昱光 <1760864645@qq.com>
2020-12-22 21:44:00 +0800

    完善客户端

--- java20/src/main/java/java20/Client.java
+++ java20/src/main/java/java20/Client.java
@@ -1,39 +1,84 @@
 package java20;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import java20.gui.MatchingGUI;
+
 public class Client {
     BufferedReader reader;
     PrintWriter writer;
     Socket sock;
-    boolean lose=false;
-    public void setUpNetworking(){
-        try{
-            sock=new Socket("127.0.0.1", 3000);
-            InputStreamReader streamReader=new InputStreamReader(sock.getInputStream());
-            reader=new BufferedReader(streamReader);
-            writer=new PrintWriter(sock.getOutputStream());
+    boolean lose = false;
+
+    public void go() {
+        setUpNetworking();
+        Thread readerThread = new Thread(new IncomingReader());
+        Thread beforePlaying = new Thread(new BeforePlaying());
+        beforePlaying.start();
+        try {
+            beforePlaying.join(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        readerThread.start();
+    }
+
+    public void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 3000);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
             System.out.println("networking established");
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public class IncomingReader implements Runnable{
-        public void run(){
+
+    public class IncomingReader implements Runnable {
+        public void run() {
             String message;
             try {
-                while(((message=reader.readLine())!=null)&&!lose){
-                    if(message.equals("Lose")){
-                        lose=true;
+                while (((message = reader.readLine()) != null) && !lose) {
+                    if (message.equals("Lose")) {
+                        lose = true;
                         break;
                     }
-                    String[] arr=message.split(",");
-                    //写文件
-                    //
+                    String[] arr = message.split(",");
+                    FileWriter fWriter = new FileWriter(new File("record.txt"));
+                    for (int i = 0; i < arr.length - 1; ++i) {
+                        fWriter.write(arr[i] + "\n");
+                    }
+                    // TODO: 解析并映射至本方屏幕
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class BeforePlaying implements Runnable {
+        public void run() {
+            String message;
+            Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    MatchingGUI matching = new MatchingGUI();
+                    matching.go();
+                }
+            });
+            thread.start();
+            try {
+                while ((message = reader.readLine()) != null) {
+                    if (message.equals("Another Player")) {
+                        System.out.println(message);
+                        break;
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
--- java20/src/main/java/java20/Main.java
+++ java20/src/main/java/java20/Main.java
@@ -8,10 +8,11 @@ import java20.warrior.Creature;
 public class Main {
     ArrayList<Creature> creatures;
     ArrayList<King> kings;
+    Client client;
     public static void main(String[] args) {
 
     }
     Main(){
-        
+        client=new Client();
     }
 }
