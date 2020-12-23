5182489c70a18858d84c14900748f06f5a34bf0e
潘昱光 <1760864645@qq.com>
2020-12-23 08:35:46 +0800

    Ability框架

---
+++ java20/src/main/java/java20/ablility/ScorpionAbility.java
@@ -0,0 +1,17 @@
+package java20.ablility;
+
+public class ScorpionAbility extends Ability {
+
+    @Override
+    public void doSth() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean canDoSth() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
