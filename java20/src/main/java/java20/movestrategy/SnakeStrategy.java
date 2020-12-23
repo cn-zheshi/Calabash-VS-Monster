8b3ebdaf9f8eace6f7fba54ce6c2f6055035cb90
潘昱光 <1760864645@qq.com>
2020-12-23 08:36:21 +0800

    Strategy框架更新

---
+++ java20/src/main/java/java20/movestrategy/SnakeStrategy.java
@@ -0,0 +1,15 @@
+package java20.movestrategy;
+
+import java.util.ArrayList;
+
+import java20.tools.Position;
+
+public class SnakeStrategy implements MoveStrategy {
+
+    @Override
+    public ArrayList<Position> positionsCanBeSet(int x, int y) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
