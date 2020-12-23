8318abc044495e773db32d8a3959270cd03c75d2
潘昱光 <1760864645@qq.com>
2020-12-23 19:18:27 +0800

    Add repaint method

--- java20/src/main/java/java20/gui/MainGUI.java
+++ java20/src/main/java/java20/gui/MainGUI.java
@@ -6,6 +6,9 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
+import java20.Board;
+import java20.tools.KindOfCreature;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -14,21 +17,39 @@ import java.awt.GridLayout;
 public class MainGUI {
     JButton[][] buttons = new JButton[10][10];
     JFrame fr = new JFrame();
+    File f;
+    ImageIcon background;
+    ImageIcon first;
+    ImageIcon second;
+    ImageIcon third;
+    ImageIcon forth;
+    ImageIcon fifth;
+    ImageIcon sixth;
+    ImageIcon seventh;
+    ImageIcon grandpa;
+    ImageIcon monster;
+    ImageIcon strongerMonster;
+    ImageIcon snake;
+    ImageIcon scropion;
 
     public static void main(String[] args) {
         new MainGUI().go();
     }
 
+    public MainGUI() {
+        f = new File(this.getClass().getResource("/").getPath());
+        background = new ImageIcon(f.getPath() + "/background.png");
+        // TODO: 加载图片资源
+    }
+
     public void go() {
         fr.setTitle("Galabash VS Monster");
-        File f = new File(this.getClass().getResource("/").getPath());
-        ImageIcon icon = new ImageIcon(f.getPath() + "/background.png");
         fr.getContentPane().setLayout(new GridLayout(10, 10));
         fr.setSize(800, 800);
         fr.setResizable(false);
         for (int y = 0; y < 10; ++y) {
             for (int x = 0; x < 10; ++x) {
-                buttons[x][y] = new JButton(icon);
+                buttons[x][y] = new JButton(background);
                 buttons[x][y].addActionListener(new ClickHandler());
                 fr.getContentPane().add(buttons[x][y]);
             }
@@ -38,10 +59,66 @@ public class MainGUI {
         fr.validate();
     }
 
+    public void repaint() {
+        for (int y = 0; y < 10; ++y) {
+            for (int x = 0; x < 10; ++x) {
+                KindOfCreature kind = Board.getBoardInstance().getVal(x, y);
+                if (kind == KindOfCreature.First) {
+                    buttons[x][y].setIcon(first);
+                    continue;
+                }
+                if (kind == KindOfCreature.Second) {
+                    buttons[x][y].setIcon(second);
+                    continue;
+                }
+                if (kind == KindOfCreature.Third) {
+                    buttons[x][y].setIcon(third);
+                    continue;
+                }
+                if (kind == KindOfCreature.Forth) {
+                    buttons[x][y].setIcon(forth);
+                    continue;
+                }
+                if (kind == KindOfCreature.Fifth) {
+                    buttons[x][y].setIcon(fifth);
+                    continue;
+                }
+                if (kind == KindOfCreature.Sixth) {
+                    buttons[x][y].setIcon(sixth);
+                    continue;
+                }
+                if (kind == KindOfCreature.Seventh) {
+                    buttons[x][y].setIcon(seventh);
+                    continue;
+                }
+                if (kind == KindOfCreature.Grandpa) {
+                    buttons[x][y].setIcon(grandpa);
+                    continue;
+                }
+                if (kind == KindOfCreature.Snake) {
+                    buttons[x][y].setIcon(snake);
+                    continue;
+                }
+                if (kind == KindOfCreature.Scorpion) {
+                    buttons[x][y].setIcon(scropion);
+                    continue;
+                }
+                if (kind == KindOfCreature.Monster) {
+                    buttons[x][y].setIcon(monster);
+                    continue;
+                }
+                if (kind == null) {
+                    buttons[x][y].setIcon(background);
+                    continue;
+                }
+            }
+        }
+    }
+
     public class ClickHandler implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            //TODO: 处理按键
+            // TODO: 处理按键
             for (int y = 0; y < 10; ++y) {
                 for (int x = 0; x < 10; ++x) {
                     if (e.getSource().equals(buttons[x][y])) {
