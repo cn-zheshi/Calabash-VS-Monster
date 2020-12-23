2255518be0caad69d35163a23ba858870e9873d7
潘昱光 <1760864645@qq.com>
2020-12-23 19:17:15 +0800

    Add Ability Framework

---
+++ java20/src/main/java/java20/ablility/FirstAbility.java
@@ -0,0 +1,17 @@
+package java20.ablility;
+
+public class FirstAbility extends Ability {
+
+    @Override
+    public void doSth() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean canDoSth() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
---
+++ java20/src/main/java/java20/ablility/SecondAbility.java
@@ -0,0 +1,17 @@
+package java20.ablility;
+
+public class SecondAbility extends Ability {
+
+    @Override
+    public void doSth() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean canDoSth() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
