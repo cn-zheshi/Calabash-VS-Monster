03edaebe1edd2a8a586b33a300569ebe0997c32d
潘昱光 <1760864645@qq.com>
2020-12-23 21:30:14 +0800

    初始化战场（部分

--- java20/src/main/java/java20/Main.java
+++ java20/src/main/java/java20/Main.java
@@ -2,38 +2,80 @@ package java20;
 
 import java.util.ArrayList;
 
+import java20.ablility.FifthAbility;
+import java20.ablility.FirstAbility;
+import java20.ablility.ForthAbility;
 import java20.ablility.GrandpaAbility;
 import java20.ablility.ScorpionAbility;
+import java20.ablility.SecondAbility;
+import java20.ablility.SeventhAbility;
+import java20.ablility.SixthAbility;
 import java20.ablility.SnakeAbility;
+import java20.ablility.ThirdAbility;
 import java20.king.King;
+import java20.movestrategy.FirstAndSecondStrategy;
+import java20.movestrategy.ForthAndFifthStrategy;
 import java20.movestrategy.GrandpaStrategy;
 import java20.movestrategy.ScorpionStrategy;
+import java20.movestrategy.SeventhStrategy;
+import java20.movestrategy.SixthStrategy;
 import java20.movestrategy.SnakeStrategy;
+import java20.movestrategy.ThirdStrategy;
 import java20.tools.KindOfCreature;
 import java20.tools.Position;
+import java20.warrior.Calabash;
 import java20.warrior.Creature;
 
 public class Main {
     ArrayList<Creature> creatures;
     ArrayList<King> kings;
     Client client;
+
     public static void main(String[] args) {
 
     }
-    Main(){
-        client=new Client();
-        Position grandpaPosition=new Position(5, 0);
-        King grandpa=new King(KindOfCreature.Grandpa, grandpaPosition, new GrandpaStrategy(), new GrandpaAbility());
+
+    Main() {
+        kings = new ArrayList<King>();
+        creatures = new ArrayList<Creature>();
+        client = new Client();
+        Position grandpaPosition = new Position(5, 0);
+        King grandpa = new King(KindOfCreature.Grandpa, grandpaPosition, new GrandpaStrategy(), new GrandpaAbility());
         kings.add(grandpa);
-        Position snakePosition=new Position(5, 9);
-        King snake=new King(KindOfCreature.Snake, snakePosition, new SnakeStrategy(), new SnakeAbility());
+        Position snakePosition = new Position(5, 9);
+        King snake = new King(KindOfCreature.Snake, snakePosition, new SnakeStrategy(), new SnakeAbility());
         kings.add(snake);
-        Position scorpionPosition=new Position(4, 9);
-        King scorpion=new King(KindOfCreature.Scorpion, scorpionPosition, new ScorpionStrategy(), new ScorpionAbility());
+        Position scorpionPosition = new Position(4, 9);
+        King scorpion = new King(KindOfCreature.Scorpion, scorpionPosition, new ScorpionStrategy(),
+                new ScorpionAbility());
         kings.add(scorpion);
-        //TODO: 添加普通妖精和七个葫芦娃
+        Position firstPosition = new Position(3, 0);
+        Calabash first = new Calabash(KindOfCreature.First, firstPosition, new FirstAndSecondStrategy(),
+                new FirstAbility());
+        creatures.add(first);
+        Position secondPosition = new Position(6, 0);
+        Calabash second = new Calabash(KindOfCreature.Second, secondPosition, new FirstAndSecondStrategy(),
+                new SecondAbility());
+        creatures.add(second);
+        Position thirdPosition = new Position(4, 1);
+        Calabash third = new Calabash(KindOfCreature.Third, thirdPosition, new ThirdStrategy(), new ThirdAbility());
+        creatures.add(third);
+        Position forthPosition = new Position(0, 1);
+        Calabash forth = new Calabash(KindOfCreature.Forth, forthPosition, new ForthAndFifthStrategy(), new ForthAbility());
+        creatures.add(forth);
+        Position fifthPosition = new Position(0, 9);
+        Calabash fifth = new Calabash(KindOfCreature.Fifth, fifthPosition, new ForthAndFifthStrategy(), new FifthAbility());
+        creatures.add(fifth);
+        Position sixthPosition = new Position(3, 2);
+        Calabash sixth = new Calabash(KindOfCreature.Sixth, sixthPosition, new SixthStrategy(), new SixthAbility());
+        creatures.add(sixth);
+        Position seventhPosition = new Position(6, 2);
+        Calabash seventh = new Calabash(KindOfCreature.Seventh, seventhPosition, new SeventhStrategy(), new SeventhAbility());
+        creatures.add(seventh);
+        // TODO: 添加普通妖精
     }
-    public void go(){
+
+    public void go() {
 
     }
 }
