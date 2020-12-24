86a87a54b62b6283fccf436af3843469d8cb6673
潘昱光 <1760864645@qq.com>
2020-12-25 00:39:25 +0800

    King也应该是生物，调整结构

---
+++ java20/src/main/java/java20/warrior/King.java
@@ -0,0 +1,27 @@
+package java20.warrior;
+
+import java.util.ArrayList;
+
+import java20.Board;
+import java20.ablility.*;
+import java20.movestrategy.MoveStrategy;
+import java20.tools.KindOfCreature;
+import java20.tools.Position;
+
+public class King extends Creature{
+    Ability ability;
+
+    public King(KindOfCreature kindOfCreature, Position p, MoveStrategy strategy, Ability ability) {
+        super(kindOfCreature, p, strategy);
+        this.ability = ability;
+    }
+
+    public boolean canUseAbility() {
+        return ability.canDoSth();
+    }
+
+    public void useAbility() {
+        ability.doSth();
+    }
+
+}
