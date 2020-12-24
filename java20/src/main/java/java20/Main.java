f5cd5f06413d50984fa1908be3dfda3726cd0ba9
潘昱光 <1760864645@qq.com>
2020-12-24 21:24:32 +0800

    修复初始位置错误

--- java20/src/main/java/java20/Main.java
+++ java20/src/main/java/java20/Main.java
@@ -55,39 +55,49 @@ public class Main {
         Position grandpaPosition = new Position(5, 0);
         King grandpa = new King(KindOfCreature.Grandpa, grandpaPosition, new GrandpaStrategy(), new GrandpaAbility());
         kings.add(grandpa);
+        Board.getBoardInstance().setVal(grandpaPosition, KindOfCreature.Grandpa);
         Position snakePosition = new Position(5, 9);
         King snake = new King(KindOfCreature.Snake, snakePosition, new SnakeStrategy(), new SnakeAbility());
         kings.add(snake);
+        Board.getBoardInstance().setVal(snakePosition, KindOfCreature.Snake);
         Position scorpionPosition = new Position(4, 9);
         King scorpion = new King(KindOfCreature.Scorpion, scorpionPosition, new ScorpionStrategy(),
                 new ScorpionAbility());
         kings.add(scorpion);
+        Board.getBoardInstance().setVal(scorpionPosition, KindOfCreature.Scorpion);
         Position firstPosition = new Position(3, 0);
         Calabash first = new Calabash(KindOfCreature.First, firstPosition, new FirstAndSecondStrategy(),
                 new FirstAbility());
         creatures.add(first);
+        Board.getBoardInstance().setVal(firstPosition, KindOfCreature.First);
         Position secondPosition = new Position(6, 0);
         Calabash second = new Calabash(KindOfCreature.Second, secondPosition, new FirstAndSecondStrategy(),
                 new SecondAbility());
         creatures.add(second);
+        Board.getBoardInstance().setVal(secondPosition, KindOfCreature.Second);
         Position thirdPosition = new Position(4, 1);
         Calabash third = new Calabash(KindOfCreature.Third, thirdPosition, new ThirdStrategy(), new ThirdAbility());
         creatures.add(third);
+        Board.getBoardInstance().setVal(thirdPosition, KindOfCreature.Third);
         Position forthPosition = new Position(0, 1);
         Calabash forth = new Calabash(KindOfCreature.Forth, forthPosition, new ForthAndFifthStrategy(),
                 new ForthAbility());
         creatures.add(forth);
-        Position fifthPosition = new Position(0, 9);
+        Board.getBoardInstance().setVal(forthPosition, KindOfCreature.Forth);
+        Position fifthPosition = new Position(9, 1);
         Calabash fifth = new Calabash(KindOfCreature.Fifth, fifthPosition, new ForthAndFifthStrategy(),
                 new FifthAbility());
         creatures.add(fifth);
+        Board.getBoardInstance().setVal(fifthPosition, KindOfCreature.Fifth);
         Position sixthPosition = new Position(3, 2);
         Calabash sixth = new Calabash(KindOfCreature.Sixth, sixthPosition, new SixthStrategy(), new SixthAbility());
         creatures.add(sixth);
+        Board.getBoardInstance().setVal(sixthPosition, KindOfCreature.Sixth);
         Position seventhPosition = new Position(6, 2);
         Calabash seventh = new Calabash(KindOfCreature.Seventh, seventhPosition, new SeventhStrategy(),
                 new SeventhAbility());
         creatures.add(seventh);
+        Board.getBoardInstance().setVal(seventhPosition, KindOfCreature.Seventh);
         // TODO: 添加普通妖精
     }
 
